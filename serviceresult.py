2457a4ee00721c9dd07c91b6bde3c1a91531a55c
Martijn Boers <martijn@plebian.nl>
2020-02-04 22:53:03 +0100

    Increase readability of method names and add method docs

---
+++ serviceresult.py
@@ -0,0 +1,11 @@
+class ServiceResult:
+    exceeds_threshold: bool
+    listeners_count: int
+    threshold: int
+    service_name: str
+
+    def __init__(self, exceeds_threshold: bool, listeners_count: int, threshold: int, service_name: str):
+        self.exceeds_threshold = exceeds_threshold
+        self.listeners_count = listeners_count
+        self.threshold = threshold
+        self.service_name = service_name
