90f531a21e693ecb18891ee40f45555b73fb3eaf
Martijn Boers <2955898+martijnboers@users.noreply.github.com>
2020-02-04 23:24:16 +0100

    Feature > Use regex to make artist detection more robust (#4)
    * Extract string templates to separate file
    
    remove some of the visual clutter in the file
    
    * Cleanup LastFM class
    
    Removed unused variables
    
    * Remove constructor from variable assignment
    
    * Use regex for matching artist names
    
    Resolves #3

--- main.py
+++ main.py
@@ -8,23 +8,30 @@ from praw import Reddit
 from config import Config
 from database import Database
 from lastfm import LastFM
+import templates
 from youtube import Youtube
 
 
 class BlotterTrax:
     reddit: Reddit = None
     useragent: str = 'BlotterTrax /r/listentothis submission bot by /u/plebianlinux'
-    config: Config = Config()
-    youtube: Youtube = Youtube()
-    last_fm: LastFM = LastFM()
-    database: Database = Database()
+    config: Config = None
+    youtube: Youtube = None
+    last_fm: LastFM = None
+    database: Database = None
     crash_timeout: int = 10
 
     def __init__(self):
         try:
+            self.config = Config()
+            self.youtube = Youtube()
+            self.last_fm = LastFM()
+            self.database = Database()
+
             self.reddit = Reddit(client_id=self.config.CLIENT_ID, client_secret=self.config.CLIENT_SECRET,
                                  password=self.config.PASSWORD, user_agent=self.useragent,
                                  username=self.config.USER_NAME)
+
         except KeyError:
             exit('Check if the configuration is set right')
 
@@ -45,63 +52,67 @@ class BlotterTrax:
             parsed_url = urlparse(url)
             youtube_service = self.youtube.get_service_result(parsed_url)
 
-            if youtube_service.exceeds_threshold:
+            if youtube_service.exceeds_threshold is True:
                 self._archive_submission_exceeding_threshold(submission, youtube_service.service_name,
                                                              youtube_service.threshold, youtube_service.listeners_count)
                 continue
 
-            artist_name = self._extract_artist_post_title(submission.title)
+            try:
+                artist_name = self._get_artist_name_from_submission_title(submission.title)
+            except LookupError:
+                self._reply_with_sticky_post(submission, templates.cant_find_artist)
+
+                continue
 
             try:
                 last_fm_service = self.last_fm.get_service_result(artist_name)
             except pylast.WSError:
                 continue
 
-            if last_fm_service.exceeds_threshold:
+            if last_fm_service.exceeds_threshold is True:
                 self._archive_submission_exceeding_threshold(submission, last_fm_service.service_name,
                                                              last_fm_service.threshold, last_fm_service.listeners_count)
                 continue
 
             # Yeey this post probably isn't breaking the rules 🌈
-            comment = submission.reply(self.last_fm.get_artist_reply(artist_name))
-            comment.mod.distinguish("yes", sticky=True)
-            self.database.save_submission(submission)
+            self._reply_with_sticky_post(submission, self.last_fm.get_artist_reply(artist_name))
 
     def _archive_submission_exceeding_threshold(self, submission, service, threshold, count):
-        reply = '''
-All apologies /u/{} but your post has been automatically removed because the artist has too many {}. The maximum is {:,}, this link has {:,}.
-If you think this is in error, please [contact the mods](https://www.reddit.com/message/compose?to=/r/listentothis&subject=Post+removed+in+error.&message=https://redd.it/{}). 
+        reply = templates.submission_exceeding_threshold.format(
+            submission.author.name, service, threshold, count, submission.id
+        )
 
-If you're new to the subreddit, please read the full list of removal reasons.
-
-_Don't blame me,_ [_I'm just a bot_](https://www.youtube.com/watch?v=jqaweMZv4Og)|[_Bugs & Code_](https://github.com/martijnboers/BlotterTrax)
-        '''.format(submission.author.name, service, threshold, count, submission.id)
-
-        comment = submission.reply(reply)
-        comment.mod.distinguish("yes", sticky=True)
-
-        submission.mod.remove()
-        self.database.save_submission(submission)
+        self._reply_with_sticky_post(submission, reply, True)
 
     def _archive_text_post_without_discussion_tag(self, submission):
-        reply = '''
-All apologies /u/{} but your post has been automatically removed because it is a text post without the [Discussion] tag 
+        reply = templates.submission_missing_discussion_tag.format(submission.author.name, submission.id)
 
-If you're new to the subreddit, please read the full list of removal reasons.
+        self._reply_with_sticky_post(submission, reply, True)
 
-_Don't blame me,_ [_I'm just a bot_](https://www.youtube.com/watch?v=jqaweMZv4Og)|[_Bugs & Code_](https://github.com/martijnboers/BlotterTrax)
-                '''.format(submission.author.name, submission.id)
-
-        comment = submission.reply(reply)
+    def _reply_with_sticky_post(self, submission, reply_text, remove=False):
+        comment = submission.reply(reply_text)
         comment.mod.distinguish("yes", sticky=True)
 
-        submission.mod.remove()
+        if remove is True:
+            submission.mod.remove()
 
         self.database.save_submission(submission)
 
     @staticmethod
-    def _extract_artist_post_title(post_title):
-        return post_title.split(' -')[0]
+    def _get_artist_name_from_submission_title(post_title):
+        artist = re.search(r'(?P<artist>.+?) \s*(-|—)\s*', post_title, re.IGNORECASE)
+        feature_artist = re.search(
+            r'(?P<artist>.+?)\s*(&|feat.?|ft\.?|feature|featuring)\s*(?P<featureArtist>.+?)\s*(-|—)\s*',
+            post_title, re.IGNORECASE
+        )
+
+        if feature_artist is not None:
+            # If there is a featuring artists, it should use the feature_artist artist result
+            return feature_artist.group('artist')
+        if artist.group('artist') is not None:
+            return artist.group('artist')
+
+        raise LookupError
 
     def daemon(self):
         try:
---
+++ tests/test_main.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from main import BlotterTrax
+
+
+class TestBlotterTrax(TestCase):
+
+    def test_extract_artist_post_title(self):
+        submissions = [
+            ('B12 - Infinite Lites (Original Mix) ', 'B12'),
+            ('Floating Points - Last Bloom ', 'Floating Points'),
+            ('Empty Spaces -- Một Cuộc Sống Khác [Vietnamese/Alternative] (2019) ', 'Empty Spaces'),
+            ('007Bonez & Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez'),
+            ('007Bonez featuring Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez'),
+            ('007Bonez feat Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez'),
+            ('007Bonez feat. Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez'),
+            ('Teen Suicide — Haunt Me x3 [indie rock] (2014)', 'Teen Suicide'),
+        ]
+        for submission_title, artist in submissions:
+            title = BlotterTrax._get_artist_name_from_submission_title(submission_title)
+
+            self.assertEqual(artist, title)
