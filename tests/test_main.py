5a643285746ec60caf6c7ba3a66b93bfaf1ee21a
TcMaX <TcMaX@users.noreply.github.com>
2020-03-02 11:49:02 +0800

    Add files via upload

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,18 +7,20 @@ class TestBlotterTrax(TestCase):
 
     def test_extract_artist_post_title(self):
         submissions = [
-            ('B12 - Infinite Lites (Original Mix) ', 'B12'),
-            ('Floating Points - Last Bloom ', 'Floating Points'),
-            ('Empty Spaces -- Một Cuộc Sống Khác [Vietnamese/Alternative] (2019) ', 'Empty Spaces'),
-            ('007Bonez & Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez'),
-            ('007Bonez featuring Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez'),
-            ('007Bonez feat Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez'),
-            ('007Bonez feat. Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez'),
-            ('Teen Suicide — Haunt Me x3 [indie rock] (2014)', 'Teen Suicide'),
-            ("upsammy - Another Place - Nous'klaer 011", 'upsammy'),
+            ('B12 - Infinite Lites (Original Mix) ', 'B12', None),
+            ('Floating Points - Last Bloom ', 'Floating Points', None),
+            ('Empty Spaces -- Một Cuộc Sống Khác [Vietnamese/Alternative] (2019) ', 'Empty Spaces', None),
+            ('007Bonez & Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro'),
+            ('007Bonez featuring Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro'),
+            ('007Bonez feat Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez feat Adro', None),
+            ('007Bonez feat. Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro'),
+            ('Teen Suicide — Haunt Me x3 [indie rock] (2014)', 'Teen Suicide', None),
+            ("upsammy - Another Place - Nous'klaer 011", 'upsammy', None),
+            ('ガールズロックバンド革命 - CHANGE', 'ガールズロックバンド革命', None),
+            ('Blume - popo -- 溺レル', 'Blume - popo', None)
         ]
-        for submission_title, artist in submissions:
+        for submission_title, artist, featuring_artist in submissions:
             title = TitleParser._get_artist_name_from_submission_title(submission_title)
-            prio_artist = TitleParser._get_prioritized_artist(title)
 
-            self.assertEqual(artist, prio_artist)
+            self.assertEqual(artist, title[0])
+            self.assertEqual(featuring_artist, title[1])
