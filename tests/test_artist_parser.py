09a91654e522c08cb306e90467f7c2962d0807ed
Martijn Boers <martijn@plebian.nl>
2020-03-03 22:59:48 +0100

    Fix tests

--- tests/test_artist_parser.py
+++ tests/test_artist_parser.py
@@ -3,13 +3,20 @@ from unittest import TestCase
 from blottertrax.helper.title_parser import TitleParser
 
 
+class MockedSubmission:
+    def __init__(self, title):
+        self.title = title
+        self.url = 'https://url.com'
+
+
 class TestBlotterTrax(TestCase):
 
     def test_extract_artist_post_title(self):
         submissions = [
             ('B12 - Infinite Lites (Original Mix) ', 'B12', None, 'Infinite Lites'),
             ('Floating Points - Last Bloom ', 'Floating Points', None, 'Last Bloom'),
-            ('Empty Spaces -- Một Cuộc Sống Khác [Vietnamese/Alternative] (2019) ', 'Empty Spaces', None, 'Một Cuộc Sống Khác'),
+            ('Empty Spaces -- Một Cuộc Sống Khác [Vietnamese/Alternative] (2019) ', 'Empty Spaces', None,
+             'Một Cuộc Sống Khác'),
             ('007Bonez & Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro', 'Motion'),
             ('007Bonez featuring Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro', 'Motion'),
             ('007Bonez feat Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez feat Adro', None, 'Motion'),
@@ -19,7 +26,7 @@ class TestBlotterTrax(TestCase):
             ('ガールズロックバンド革命 - CHANGE', 'ガールズロックバンド革命', None, 'CHANGE'),
         ]
         for submission_title, artist, featuring_artist, song_title in submissions:
-            title = TitleParser.create_parsed_submission_from_submission(submission_title)
+            title = TitleParser.create_parsed_submission_from_submission(MockedSubmission(submission_title))
 
             self.assertEqual(artist, title.artist)
             self.assertEqual(featuring_artist, title.featuring_artist)
