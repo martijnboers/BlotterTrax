27ed90679ea54cd06d517a2fe1db34a8a7ccf3a2
Chris Heath <cheath@spectrumsi.com>
2020-03-02 21:24:29 -0600

    Supply link to rules in the removal message.

--- templates.py
+++ templates.py
@@ -1,8 +1,8 @@
 submission_exceeding_threshold: str = '''
-All apologies /u/{} but your post has been automatically removed because the artist has too many {}. The maximum is {:,}, this link has {:,}.
+Our apologies /u/{} but your post has been automatically removed because the artist has too many {}. The maximum is {:,} and this link has {:,}.
 If you think this is in error, please [contact the mods](https://www.reddit.com/message/compose?to=/r/listentothis&subject=Post removed in error.&message=https://redd.it/{}). 
 
-If you're new to the subreddit, please read the full list of removal reasons.
+If you're new to the subreddit, please [read the full subreddit rules](https://www.reddit.com/r/listentothis/wiki/rules).
 
 _Don't blame me,_ [_I'm just a bot_](https://www.youtube.com/watch?v=jqaweMZv4Og)|[_Bugs & Code_](https://github.com/martijnboers/BlotterTrax)
 '''
