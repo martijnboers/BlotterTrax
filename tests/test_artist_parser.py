a7934a798783ce9c301b0942c1262b295d7f2bd8
TcMaX <TcMaX@users.noreply.github.com>
2020-03-06 00:10:24 +0800

    updated test to reflect new priorities

--- tests/test_artist_parser.py
+++ tests/test_artist_parser.py
@@ -15,16 +15,16 @@ class TestBlotterTrax(TestCase):
             ('B12 - Infinite Lites (Original Mix) ', 'B12', None, 'Infinite Lites'),
             ('Floating Points - Last Bloom ', 'Floating Points', None, 'Last Bloom'),
             ('Empty Spaces -- Một Cuộc Sống Khác [Vietnamese/Alternative] (2019) ', 'Empty Spaces', None, 'Một Cuộc Sống Khác'),
-            ('007Bonez & Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro', 'Motion'),
             ('007Bonez featuring Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro', 'Motion'),
-            ('007Bonez feat Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez feat Adro', None, 'Motion'),
+            ('007Bonez feat Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro', 'Motion'),
             ('007Bonez feat. Adro - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro', 'Motion'),
             ('007Bonez - Motion featuring Adro [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro', 'Motion'),
             ('007Bonez (featuring Adro) - Motion [Hip-Hop / Rap] (2019)', '007Bonez', 'Adro', 'Motion'),
             ('Teen Suicide — Haunt Me x3 [indie rock] (2014)', 'Teen Suicide', None, 'Haunt Me x3'),
             ("upsammy - Another Place - Nous'klaer 011", 'upsammy', None, "Another Place - Nous'klaer 011"),
             ('ガールズロックバンド革命 - CHANGE', 'ガールズロックバンド革命', None, 'CHANGE'),
-            ('Blume - popo -- 溺レル', 'Blume - popo', None, '溺レル')
+            ('Blume - popo -- 溺レル', 'Blume - popo', None, '溺レル'),
+            ('Simon & Garfunkel - The Sound of Silence', 'Simon & Garfunkel', None, 'The Sound of Silence')
         ]
         for submission_title, artist, featuring_artist, song_name in submissions:
             title = TitleParser.get_artist_name_from_submission_title(submission_title)
