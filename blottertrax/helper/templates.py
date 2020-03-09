a5a7cbf9d98e0486b19fa17c1e8ef47abfde604f
TcMaX <TcMaX@users.noreply.github.com>
2020-03-09 10:18:14 +0800

    Added template

--- blottertrax/helper/templates.py
+++ blottertrax/helper/templates.py
@@ -4,7 +4,16 @@ If you think this is in error, please [contact the mods](https://www.reddit.com/
 
 If you're new to the subreddit, please [read the full subreddit rules](https://www.reddit.com/r/listentothis/wiki/rules).
 
+
 _Don't blame me, I'm just a bot_ | [_Bugs & Code_](https://github.com/martijnboers/BlotterTrax)
+'''
+
+submission_repost: str = '''
+>**Artist Reposting**
+
+This post has been removed due to the artist having been posted too recently. No artist may be posted more than once a week and you may not use the same song within a 30 day period. Artists with posts that score more than 100pts may not be posted for the next month; artists that gain multiple posts of 100pts will be put on a cooldown for up to 90 days.
+
+A full explanation of this rule can be found [here](https://www.reddit.com/r/listentothis/wiki/rules).
 '''
 
 reply_with_last_fm_info: str = '''
