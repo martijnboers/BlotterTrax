6f02e01958fa271d835a65907e433fa9d05b3fc7
Martijn Boers <martijn@plebian.nl>
2020-04-14 21:34:09 +0200

    Apply whitelist to socials
    The bot is probably banned because of posting too many weird links. I
    think using a whitelist it won't result in a ban but maybe this has to
    be removed completely

--- blottertrax/description_provider.py
+++ blottertrax/description_provider.py
@@ -9,6 +9,9 @@ from blottertrax.value_objects.parsed_submission import ParsedSubmission
 
 class DescriptionProvider:
     musicbrainz = None
+    whitelist = ['twitter.com', 'facebook.com', 'instagram.com', 'allmusic.com', 'bandcamp.com',
+                 'bandsintown.com', 'discogs.com', 'genius.com', 'rateyourmusic.com', 'last.fm',
+                 'soundcloud.com', 'youtube.com', 'www.whosampled.com']
 
     def __init__(self):
         config = Config()
@@ -33,9 +36,10 @@ class DescriptionProvider:
         album_release_date = ArrayUtil.safe_list_get(recording, False, 'release-list', 0, 'date')
         life_span_begin = ArrayUtil.safe_list_get(artist, '?', 'life-span', 'begin')
         life_span_end = ArrayUtil.safe_list_get(artist, 'now', 'life-span', 'end')
-        has_life_span = (life_span_end != 'now' or life_span_begin != '?')
-        has_tags = (ArrayUtil.safe_list_get(artist, False, 'tag-list') is not False)
-        has_socials = (ArrayUtil.safe_list_get(artist, False, 'url-relation-list') is not False)
+
+        has_life_span = life_span_end != 'now' or life_span_begin != '?'
+        has_tags = ArrayUtil.safe_list_get(artist, False, 'tag-list') is not False
+        has_socials = ArrayUtil.safe_list_get(artist, False, 'url-relation-list') is not False
 
         if has_tags is False and has_socials is False:
             raise DescriptionException('Neither tags nor socials found, skipping')
@@ -43,8 +47,10 @@ class DescriptionProvider:
         album_release_date = '' if album_release_date is False else '({})'.format(album_release_date)
         life_span = '' if has_life_span is False else '({} to {})'.format(life_span_begin, life_span_end)
         tags = ', '.join(map(lambda t: t['name'], artist['tag-list'][:5])) if has_tags else 'none'
+
         socials = ', '.join(
-            map(self.format_network_to_friendly_name, artist['url-relation-list'])) if has_socials else 'none'
+            map(self.format_network_to_friendly_name, filter(self.apply_whitelist, artist['url-relation-list']))
+        ) if has_socials else 'none'
 
         return templates.musicbrainz_artist_info.strip().format(
             artist['name'],
@@ -52,7 +58,7 @@ class DescriptionProvider:
             recording['title'],
             album_release_date,
             tags,
-            socials,
+            'none' if not socials else socials,
             artist['id']
         )
 
@@ -60,6 +66,13 @@ class DescriptionProvider:
         return self.musicbrainz.get_artist_by_id(
             id=artist_id, includes=['tags', 'ratings', 'annotation', 'url-rels', 'user-tags']
         )['artist']
+
+    @staticmethod
+    def apply_whitelist(info) -> bool:
+        for domain in DescriptionProvider.whitelist:
+            if domain in info['target']:
+                return True
+        return False
 
     @staticmethod
     def format_network_to_friendly_name(info) -> str:
